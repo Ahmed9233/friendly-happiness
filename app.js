d9d351dab9d041c88e5eab9cacd4b0162143ea91
Ahmed9233 <125299424+Ahmed9233@users.noreply.github.com>
2023-04-10 14:29:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// let myPara = document.getElementById('Para').innerText = "Pakistan Zindabad";
+// console.log(myPara);
+
+// => 1
+
+// function Ahmed(num) {
+//     return num + 5
+// }
+
+// let pak = Ahmed(10);
+// console.log(pak);
+
+// => 2
+
+let ab = ["Ahmed", "Iklhas", "Hamza", "Mubashir"]
+
+function saqib(req) {
+    if (req === "Ahmed") { console.log("Found"); }
+    else if (req === "Ahmed") { console.log("Found"); }
+    else if (req === "Hamza") { console.log("Found"); }
+    else if (req === "Iklhas") { console.log("Found"); }
+    else if (req === "Mubashir") { console.log("Found"); }
+    else { console.log("Not Found"); };
+}
+saqib("Mubashir");
